53bc4f4b7048b30e585ee9f4a334871827397492
AntonMuzychenko <49153700+AntonMuzychenko@users.noreply.github.com>
2019-07-19 10:02:24 +0300

    Updates

---
+++ precache-manifest.942ea2e8d6f9adf78e0399ed53084dc5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc8ac7ee395e267046892c868e2c961",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/goit-react-hw-02-reader/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3a061f220f991a8c94db",
+    "url": "/goit-react-hw-02-reader/static/js/main.3c1910d1.chunk.js"
+  },
+  {
+    "revision": "0074b703c3097bd9a1db",
+    "url": "/goit-react-hw-02-reader/static/js/runtime~main.da19d381.js"
+  }
+]);
