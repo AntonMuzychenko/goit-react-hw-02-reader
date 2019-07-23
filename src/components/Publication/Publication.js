1cde404d17d894d3b64b0eecfabfcb4b9a8a844c
AntonMuzychenko <49153700+AntonMuzychenko@users.noreply.github.com>
2019-07-23 15:44:11 +0300

    test

--- src/components/Publication/Publication.js
+++ src/components/Publication/Publication.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Publication.module.css';
 
-const Publication = () => {
+const Publication = ({ title, text }) => {
   return (
-    <article className="publication">
-      <h2>Lorem ipsum dolor sit amet.</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
-        molestiae dolore ipsa sed similique necessitatibus. Aut qui porro
-        quibusdam esse libero est eius, repellendus unde nihil, sequi voluptate
-        eaque officiis aliquam impedit laborum adipisci cumque sit.
-      </p>
-    </article>
+    <div className={styles.publication}>
+      <h2 className={styles.title}>{title}</h2>
+      <p className={styles.text}>{text}</p>
+    </div>
   );
+};
+
+Publication.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default Publication;
